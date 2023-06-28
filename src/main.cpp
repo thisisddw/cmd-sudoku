d2a696a6ad711f89a0f2c9329b9be9461556a27c
ephratica <ephratica@foxmail.com>
2023-06-28 17:57:03 +0800

    print to screen and files

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,51 @@ int main(int argc, char *argv[])
             goto ARG_ERR;
 
         auto list = generate_c(arguments.number);
+
+        // print to screen
         for(const auto &s: list)
             printf("%s\n", s.to_pretty_string().c_str());
+        // print to ending.txt
+        if(std::freopen("../ending.txt", "w", stdout)) 
+        {
+            for(const auto &s: list)
+                std::printf("%s\n", s.to_string().c_str());
+            std::fclose(stdout);
+        }
+        else
+        {
+            printf("print to file error!\n");
+        }
+    }
+    if(arguments.flags & Arguments::S_FLAG)
+    {
+        if(arguments.flags != Arguments::S_FLAG)
+            goto ARG_ERR;
+
+        
+    }
+    if(arguments.flags & Arguments::N_FLAG)
+    {
+        if(arguments.flags == Arguments::N_FLAG)
+        {
+            auto list = generate_n(arguments.number);
+
+            // print to screen
+            for(const auto &s: list)
+                printf("%s\n", s.to_pretty_string().c_str());
+            
+            // print to game.txt
+            if(std::freopen("../game.txt", "w", stdout)) 
+            {
+                for(const auto &s: list)
+                    std::printf("%s\n", s.to_string().c_str());
+                std::fclose(stdout);
+            }
+            else
+            {
+                printf("print to file error!\n");
+            }
+        }
     }
 
 
